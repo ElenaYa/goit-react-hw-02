f58c79b2734f9b5ceca13a3b58703f158e73c410
ElenaYa <elena.yatsynyuk@gmail.com>
2024-03-11 23:38:59 +0200

    p

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,8 +4,7 @@ const Description = () => {
     return (
         <div>
             <h1>Sip Happens Café</h1>
-            <p className={css.text}>Please leave your feedback about our service by selecting one of the options below.
-            </p>
+            <p className={css.text}>Please leave your feedback about our service by selecting one of the options below.</p>
         </div>
     );
 };
